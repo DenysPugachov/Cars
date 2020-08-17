f1d6266068c4e329853afd94cddda5540c0f6722
Denys <pugachovdenys@gmail.com>
2020-08-17 12:24:45 +0300

    guard isLoggedIn to about page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Cars from "./Cars/Cars";
 import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+    state = {
+        isLoggedIn: false,
+    };
+
     render() {
         return (
             <div>
@@ -37,6 +41,15 @@ class App extends Component {
                         </li>
                     </ul>
                 </nav>
+                <hr />
+                <div style={{ textAlign: "center" }}>
+                    <h3>
+                        Is logged in {this.state.isLoggedIn ? "TRUE" : "FALSE"}
+                    </h3>
+                    <button onClick={() => this.setState({ isLoggedIn: true })}>
+                        Login
+                    </button>
+                </div>
 
                 <hr />
                 {/*localhost:3000*/}
@@ -45,7 +58,10 @@ class App extends Component {
                 <Switch>
                     {/* <Route path="/" exact render={() => <h1>Home page</h1>} /> */}
                     <Route path="/" exact render={() => <h1> Home page</h1>} />
-                    <Route path="/about" component={About} />
+
+                    {this.state.isLoggedIn ? (
+                        <Route path="/about" component={About} />
+                    ) : null}
                     <Route path="/cars/:name" component={CarDetail} />
                     <Route path="/cars" component={Cars} />
                     <Redirect to={"/"} />
